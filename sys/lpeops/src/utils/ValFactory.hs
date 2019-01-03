6160daef0911b127b4954766e2aee366e9de97a1
Djurre <djurrevanderwal@gmail.com>
2019-01-03 15:35:33 +0100

    Improved default value generation (more support for recursive sorts)

--- sys/lpeops/src/utils/ValFactory.hs
+++ sys/lpeops/src/utils/ValFactory.hs
@@ -15,8 +15,8 @@ See LICENSE at root directory of this repository.
 -----------------------------------------------------------------------------
 
 module ValFactory (
-sort2defaultConst,
-sort2defaultValue
+sort2defaultValue,
+sort2defaultConst
 ) where
 
 import qualified Data.List as List
@@ -28,6 +28,9 @@ import qualified ValExpr
 import qualified SortId
 import qualified Constant
 import qualified CstrId
+
+sort2defaultValue :: TxsDefs.TxsDefs -> SortId.SortId -> TxsDefs.VExpr
+sort2defaultValue tdefs sortId = ValExpr.cstrConst (sort2defaultConst tdefs sortId)
 
 sort2defaultConst :: TxsDefs.TxsDefs -> SortId.SortId -> Constant.Constant
 sort2defaultConst tdefs sortId
@@ -41,28 +44,35 @@ sort2defaultConst tdefs sortId
         Constant.Cstring (Text.pack "")
     | otherwise =
         -- Use any non-recursive constructor of this sort to express a value of this sort:
-        case [ cstrId | cstrId <- Map.keys (TxsDefs.cstrDefs tdefs), CstrId.cstrsort cstrId == sortId, not(isRecursiveCstr tdefs cstrId) ] of
+        case [ cstrId | cstrId <- Map.keys (TxsDefs.cstrDefs tdefs), CstrId.cstrsort cstrId == sortId, isNonRecursiveCstr tdefs (Set.singleton cstrId) cstrId ] of
+          -- Do the same for the parameters:
           (cstrId:_) -> Constant.Ccstr cstrId (map (sort2defaultConst tdefs) (CstrId.cstrargs cstrId))
           [] -> error ("Failed to generate a default value for " ++ show sortId ++ " (available={" ++ List.intercalate ", " (map show (Map.keys (TxsDefs.cstrDefs tdefs))) ++ "})!")
 -- sort2defaultConst
 
-sort2defaultValue :: TxsDefs.TxsDefs -> SortId.SortId -> TxsDefs.VExpr
-sort2defaultValue tdefs sortId = ValExpr.cstrConst (sort2defaultConst tdefs sortId)
+-- Determines if the specified constructor is non-recursive, meaning that
+-- all of its arguments are of sorts that are non-recursive.
+isNonRecursiveCstr :: TxsDefs.TxsDefs -> Set.Set CstrId.CstrId -> CstrId.CstrId -> Bool
+isNonRecursiveCstr tdefs beenHere cstrId =
+    if Set.member cstrId beenHere
+    then True
+    else List.all (isNonRecursiveSort tdefs (Set.insert cstrId beenHere)) (CstrId.cstrargs cstrId)
+-- isNonRecursiveCstr
 
-isRecursiveCstr :: TxsDefs.TxsDefs -> CstrId.CstrId -> Bool
-isRecursiveCstr tdefs cstrId = List.any (isRecursiveSort tdefs Set.empty) (CstrId.cstrargs cstrId)
-
-isRecursiveSort :: TxsDefs.TxsDefs -> Set.Set SortId.SortId -> SortId.SortId -> Bool
-isRecursiveSort tdefs beenHere sortId
-    | sortId == SortId.sortIdBool = False
-    | sortId == SortId.sortIdInt = False
-    | sortId == SortId.sortIdString = False
-    | sortId == SortId.sortIdRegex = False
+-- Checks if the specified sort is non-recursive, meaning that
+--  - The sort is a primitive sort (bool, int, string, regex); or
+--  - The sort is a constructor sort with at least one non-recursive constructor.
+isNonRecursiveSort :: TxsDefs.TxsDefs -> Set.Set CstrId.CstrId -> SortId.SortId -> Bool
+isNonRecursiveSort tdefs beenHere sortId
+    | sortId == SortId.sortIdBool = True
+    | sortId == SortId.sortIdInt = True
+    | sortId == SortId.sortIdString = True
+    | sortId == SortId.sortIdRegex = True
     | otherwise =
+        -- Obtain a list of all constructors that result in the specified sort:
         let sortCstrs = [ cstrId | cstrId <- Map.keys (TxsDefs.cstrDefs tdefs), CstrId.cstrsort cstrId == sortId ] in
-        let sortCstrParamSorts = concatMap CstrId.cstrargs sortCstrs in
-          Set.member sortId beenHere || List.any (isRecursiveSort tdefs (Set.insert sortId beenHere)) sortCstrParamSorts
--- isRecursiveSort
+            List.any (isNonRecursiveCstr tdefs beenHere) sortCstrs
+-- isNonRecursiveSort
 
 
 
