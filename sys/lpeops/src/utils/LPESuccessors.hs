c808fdbe1bc066f47b9a1cda85189a518becf6da
Djurre <djurrevanderwal@gmail.com>
2019-01-10 11:30:43 +0100

    Cleaned up code that checks whether a summand can be a successor of another summand, and more

--- sys/lpeops/src/utils/LPESuccessors.hs
+++ sys/lpeops/src/utils/LPESuccessors.hs
@@ -15,68 +15,64 @@ See LICENSE at root directory of this repository.
 -----------------------------------------------------------------------------
 
 module LPESuccessors (
-getPossiblePredecessors,
-couldHavePredecessor,
+isPossibleSuccessor,
 getPossibleSuccessors,
-getDefiniteSuccessors
+isDefiniteSuccessor,
+getDefiniteSuccessors,
+isPossiblePredecessor,
+getPossiblePredecessors,
+couldHavePredecessor
 ) where
 
 import qualified Control.Monad       as Monad
+import qualified Data.List           as List
 import qualified Data.Set            as Set
 import qualified EnvCore             as IOC
+import qualified Satisfiability      as Sat
 import qualified TxsDefs
-import qualified Satisfiability as Sat
 import qualified ValExpr
 import           LPETypes
 import           BlindSubst
 
-getPossiblePredecessors :: LPESummands -> TxsDefs.VExpr -> LPESummand -> IOC.IOC [LPESummand]
-getPossiblePredecessors allSummands invariant (LPESummand _ _ guard _) =
-    Monad.foldM addSummandIfPossiblePredecessor [] (Set.toList allSummands)
-  where
-    addSummandIfPossiblePredecessor :: [LPESummand] -> LPESummand -> IOC.IOC [LPESummand]
-    addSummandIfPossiblePredecessor soFar summand@(LPESummand _ _ g paramEqs) = do
-        guard' <- doBlindSubst paramEqs guard
-        sat <- Sat.couldBeSatisfiable (ValExpr.cstrAnd (Set.fromList [g, guard'])) invariant
-        return $ if sat then summand:soFar else soFar
--- getPossibleSuccessors
-
-couldHavePredecessor :: LPESummands -> TxsDefs.VExpr -> LPESummand -> IOC.IOC Bool
-couldHavePredecessor allSummands invariant (LPESummand _ _ guard _) =
-    Monad.foldM isPossiblePredecessor False (Set.toList allSummands)
-  where
-    isPossiblePredecessor :: Bool -> LPESummand -> IOC.IOC Bool
-    isPossiblePredecessor soFar (LPESummand _ _ g paramEqs) =
-        if soFar
-        then return True
-        else do guard' <- doBlindSubst paramEqs guard
-                Sat.couldBeSatisfiable (ValExpr.cstrAnd (Set.fromList [g, guard'])) invariant
--- getPossibleSuccessors
+isPossibleSuccessor :: LPESummand -> TxsDefs.VExpr -> LPESummand -> IOC.IOC Bool
+isPossibleSuccessor summand invariant candidate = do
+    g <- doBlindSubst (lpeSmdEqs summand) (lpeSmdGuard candidate)
+    Sat.couldBeSatisfiable (ValExpr.cstrAnd (Set.fromList [lpeSmdGuard summand, g])) invariant
+-- isPossibleSuccessor
 
 -- Selects all potential successors summands of a given summand from a list with all summands.
 -- (In actuality, an overapproximation of all potential successors is selected, namely those
 -- whose guard can be satisfied after the guard of the current summand has been satisfied and
 -- after the substitutions of the process recursion have taken place.)
 getPossibleSuccessors :: LPESummands -> TxsDefs.VExpr -> LPESummand -> IOC.IOC [LPESummand]
-getPossibleSuccessors allSummands invariant (LPESummand _ _ guard paramEqs) =
-    Monad.foldM addSummandIfPossibleSuccessor [] (Set.toList allSummands)
-  where
-    addSummandIfPossibleSuccessor :: [LPESummand] -> LPESummand -> IOC.IOC [LPESummand]
-    addSummandIfPossibleSuccessor soFar summand@(LPESummand _ _ g _) = do
-        g' <- doBlindSubst paramEqs g
-        sat <- Sat.couldBeSatisfiable (ValExpr.cstrAnd (Set.fromList [guard, g'])) invariant
-        return $ if sat then soFar ++ [summand] else soFar
+getPossibleSuccessors summands invariant summand =
+    Monad.filterM (isPossibleSuccessor summand invariant) (Set.toList summands)
 -- getPossibleSuccessors
+
+isDefiniteSuccessor :: LPESummand -> TxsDefs.VExpr -> LPESummand -> IOC.IOC Bool
+isDefiniteSuccessor summand invariant candidate = do
+    g <- doBlindSubst (lpeSmdEqs summand) (lpeSmdGuard candidate)
+    Sat.isTautology (ValExpr.cstrAnd (Set.fromList [lpeSmdGuard summand, g])) invariant
+-- isDefiniteSuccessor
 
 -- Selects all summands from a given list that are definitely successors of a given summand.
 -- The result is an underapproximation!
 getDefiniteSuccessors :: LPESummands -> TxsDefs.VExpr -> LPESummand -> IOC.IOC [LPESummand]
-getDefiniteSuccessors allSummands invariant (LPESummand _ _ guard paramEqs) =
-    Monad.filterM isDefiniteSuccessor (Set.toList allSummands)
-  where
-    isDefiniteSuccessor :: LPESummand -> IOC.IOC Bool
-    isDefiniteSuccessor (LPESummand _ _ g _) = do
-        g' <- doBlindSubst paramEqs g
-        Sat.isTautology (ValExpr.cstrAnd (Set.fromList [guard, g'])) invariant
+getDefiniteSuccessors summands invariant summand =
+    Monad.filterM (isDefiniteSuccessor summand invariant) (Set.toList summands)
 -- getDefiniteSuccessors
+
+isPossiblePredecessor :: LPESummand -> TxsDefs.VExpr -> LPESummand -> IOC.IOC Bool
+isPossiblePredecessor summand invariant candidate = isPossibleSuccessor candidate invariant summand
+
+getPossiblePredecessors :: LPESummands -> TxsDefs.VExpr -> LPESummand -> IOC.IOC [LPESummand]
+getPossiblePredecessors summands invariant summand =
+    Monad.filterM (isPossiblePredecessor summand invariant) (Set.toList summands)
+-- getPossiblePredecessors
+
+couldHavePredecessor :: LPESummands -> TxsDefs.VExpr -> LPESummand -> IOC.IOC Bool
+couldHavePredecessor summands invariant summand = do
+    preds <- getPossiblePredecessors summands invariant summand
+    return (not (List.null preds))
+-- couldHavePredecessor
 
