83b7902311e6f233e16875a29daaceda4aa5a1fa
Djurre <djurrevanderwal@gmail.com>
2019-01-14 09:43:40 +0100

    Added a way to export multiple files while executing a chain of LPE operations

--- sys/lpeops/src/core/LPEOps.hs
+++ sys/lpeops/src/core/LPEOps.hs
@@ -54,6 +54,18 @@ lpeOpsVersion = "2019.01.07.03"
 
 data LPEOp = LPEOpLoop | LPEOp LPEOperation
 
+insertAtToken :: String -> String -> String
+insertAtToken _ [] = []
+insertAtToken [] list = list
+insertAtToken insertion list = search [] list
+  where
+    search :: String -> String -> String
+    search beenHere [] = beenHere
+    search beenHere [x] = beenHere ++ [x]
+    search beenHere ('%':'i':xs) = search (beenHere ++ insertion) xs
+    search beenHere (x:xs) = search (beenHere ++ [x]) xs
+-- insertAtToken
+
 -- Core method that does the following:
 --  1. Transforms a closed process expression to an LPE;
 --  2. Applies the given operations to the LPE, which results in a new LPE;
@@ -63,31 +75,31 @@ lpeOperations operations modelId out invariant = do
     msgsOrLpe <- model2lpe modelId
     case msgsOrLpe of
       Left msgs -> return (Left msgs)
-      Right lpe -> do msgsOrNewLpe <- lpeOperation operations operations [lpe, lpe] out invariant
+      Right lpe -> do msgsOrNewLpe <- lpeOperation operations 1 operations [lpe, lpe] out invariant
                       case msgsOrNewLpe of
                         Left msgs -> return (Left msgs)
                         Right [] -> return (Left ["No output LPE found!"]) -- Should not happen.
                         Right (newLpe:_) -> do if newLpe /= lpe
-                                               then IOC.putMsgs [ EnvData.TXS_CORE_ANY "LPE has been rewritten!" ]
-                                               else IOC.putMsgs [ EnvData.TXS_CORE_ANY "LPE is identical to input!" ]
-                                               temp <- lpe2model (newLpe { lpeName = Text.pack out })
+                                               then IOC.putMsgs [ EnvData.TXS_CORE_ANY "( LPE has been rewritten! )" ]
+                                               else IOC.putMsgs [ EnvData.TXS_CORE_ANY "( LPE is identical to input! )" ]
+                                               temp <- lpe2model (newLpe { lpeName = Text.pack (insertAtToken "" out) })
                                                return (Right temp)
 -- lpeOperations
 
-lpeOperation :: [LPEOp] -> [LPEOp] -> [LPE] -> String -> TxsDefs.VExpr -> IOC.IOC (Either [String] [LPE])
-lpeOperation _ops _ [] _out _invariant = return (Left ["No input LPE found!"])
-lpeOperation _ops [] lpeInstances _out _invariant = return (Right lpeInstances)
-lpeOperation ops (LPEOpLoop:xs) (lpe:ys) out invariant =
+lpeOperation :: [LPEOp] -> Int -> [LPEOp] -> [LPE] -> String -> TxsDefs.VExpr -> IOC.IOC (Either [String] [LPE])
+lpeOperation _ops _ _ [] _out _invariant = return (Left ["No input LPE found!"])
+lpeOperation _ops _ [] lpeInstances _out _invariant = return (Right lpeInstances)
+lpeOperation ops i (LPEOpLoop:xs) (lpe:ys) out invariant =
     if lpe `elem` ys
-    then lpeOperation ops xs (lpe:ys) out invariant
-    else lpeOperation ops ops (lpe:lpe:ys) out invariant
-lpeOperation ops (LPEOp op:xs) (lpe:ys) out invariant = do
-    msgsOrNewLpe <- op lpe out invariant
+    then lpeOperation ops i xs (lpe:ys) (insertAtToken (show i) out) invariant
+    else lpeOperation ops (i + 1) ops (lpe:lpe:ys) (insertAtToken (show i) out) invariant
+lpeOperation ops i (LPEOp op:xs) (lpe:ys) out invariant = do
+    msgsOrNewLpe <- op lpe (insertAtToken (show i) out) invariant
     case msgsOrNewLpe of
       Left msgs -> return (Left msgs)
       Right newLpe -> let problems = validateLPEModel newLpe in
                         if null problems
-                        then lpeOperation ops xs (newLpe:ys) out invariant
+                        then lpeOperation ops i xs (newLpe:ys) out invariant
                         else do IOC.putMsgs [ EnvData.TXS_CORE_ANY ("lpe = " ++ showLPE newLpe) ]
                                 return (Left problems)
 -- lpeOperation
